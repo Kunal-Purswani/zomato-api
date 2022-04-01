166cf7135e9f1665adbf856a76948632b4331abb
Kunal Purswani <kunalpurswani40@gmail.com>
2022-04-02 00:07:31 +0530

    Added create user functionality

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express()
 
 require('./configs/mongodb.config')
 
+const userRoute = require('./routes/user.route')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/createUser', userRoute);
+
 app.get('/',(req,res)=>{
     res.send("This is Zomato API")
 })
---
+++ models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
+},
+{ versionKey: false }
+)
+
+module.exports = mongoose.model('users', userSchema);
